7e16d1cbc9f948edb0c26016baf9660c66304c8a
Jagadishabi <jagadaizy@gmail.com>
2022-01-09 17:57:32 +0530

    Done

---
+++ src/org/com/GitPush.java
@@ -0,0 +1,25 @@
+package org.com;
+
+public class GitPush {
+	
+	public void Java() {
+
+System.out.println("java completed");
+	}
+public void Selenium() {
+System.out.println("Selenium completed");
+}
+public void Framework() {
+System.out.println("Framework is on going");
+}
+
+public static void main(String[] args) {
+	
+GitPush g = new GitPush();
+
+g.Java();
+g.Selenium();
+g.Framework();
+	
+}
+}
